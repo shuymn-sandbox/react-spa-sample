5d97a45ec4b1f497bc6cfe1a4d42f7f73d2fb535
shuymn <ndozto@gmail.com>
2017-09-11 01:27:59 +0900

    Remove a unused library

--- resources/assets/js/components/Header.js
+++ resources/assets/js/components/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppBar, withStyles } from 'material-ui';
+import { AppBar } from 'material-ui';
 
 export default class Header extends Component {
     render() {
